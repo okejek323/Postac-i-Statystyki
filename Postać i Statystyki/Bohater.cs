e55b2fd8afc9308fad6c13f5f24425f07a1f048d
User <User@DESKTOP-CF1AR4D>
2025-10-09 13:46:28 +0200

    Dodaj pliki projektów.

---
+++ Postać i Statystyki/Bohater.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postać_i_Statystyki
+{
+    internal class Bohater
+    {
+        public string imie;
+        public string klasa;
+        private int zdrowie = 100;
+        private int mana = 50;
+
+        public void Osoba(string imie, string klasa)
+        {
+            Console.WriteLine($"{imie} - {klasa} \n Zdrowie - {zdrowie} \n Mana - {mana} \n");
+        }
+    }
+}
---
+++ Postać i Statystyki/Program.cs
@@ -0,0 +1,7 @@
+﻿using Postać_i_Statystyki;
+
+Bohater Bohater_A = new Bohater();
+Bohater Bohater_B = new Bohater();
+
+Bohater_A.Osoba("Tomek", "Wojownik");
+Bohater_B.Osoba("Michał", "Mag");
