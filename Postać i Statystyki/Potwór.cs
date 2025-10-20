38c032fb7a1c83c9e320ceb2a8e8d65e0a1ab757
okejek323 <144665821+okejek323@users.noreply.github.com>
2025-10-20 13:55:17 +0200

    Add files via upload

---
+++ Postać i Statystyki/Potwór.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postać_i_Statystyki
+{
+    internal abstract class Potwór
+    {
+        public string nazwa;
+        public int zdrowie;
+
+        public abstract void wydajDzwiek();
+
+        public class Ork : Potwór 
+        {
+            public override void wydajDzwiek() 
+            {
+                Console.WriteLine($"FOR THE HORDE!");
+            }
+        }
+
+        public class Goblin : Potwór 
+        {
+            public override void wydajDzwiek()
+            {
+                Console.WriteLine($"Squeak!");
+            }
+        }
+    }
+}
