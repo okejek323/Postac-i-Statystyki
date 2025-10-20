e5514d3a47c4fa8410cd2c7ea3e2b1cd17939712
okejek323 <144665821+okejek323@users.noreply.github.com>
2025-10-20 09:32:38 +0200

    Bohater.cs

--- Postać i Statystyki/Bohater.cs
+++ Postać i Statystyki/Bohater.cs
@@ -13,6 +13,20 @@ namespace Postać_i_Statystyki
         public string klasa;
         private int zdrowie = 100;
         private int mana = 50;
+        public int getZdrowie { get; }
+        public int setZdrowie(int obrażenia)
+        {
+            if (zdrowie >= 0)
+            {
+                zdrowie -= obrażenia;
+                if (zdrowie <= 0)
+                {
+                    zdrowie = 0;
+                    Console.WriteLine($"Postać została pokonana");
+                }
+            }
+            return zdrowie;
+        }
 
         public void Osoba(string imie, string klasa)
         {
