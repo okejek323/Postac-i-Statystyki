32aa1afd99851df40a36360b43597cdeb9aacf15
okejek323 <144665821+okejek323@users.noreply.github.com>
2025-10-20 13:55:52 +0200

    Update Program.cs

--- Postać i Statystyki/Program.cs
+++ Postać i Statystyki/Program.cs
@@ -3,8 +3,21 @@
 Bohater Bohater_A = new Bohater.Wojownik();
 Bohater Bohater_B = new Bohater.Czarodziej();
 
+Potwór Potwór_A = new Potwór.Ork();
+Potwór Potwór_B = new Potwór.Goblin();
+
 Bohater_B.Atakuj(Bohater_A);
 Bohater_A.Atakuj(Bohater_B);
 
 Bohater_A.Osoba("Tomek", "Wojownik");
 Bohater_B.Osoba("Michał", "Mag");
+
+List<Potwór> Potwory = new List<Potwór>();
+
+Potwory.Add(new Potwór.Ork());
+Potwory.Add(new Potwór.Goblin());
+
+foreach(var potwór in Potwory)
+{
+    potwór.wydajDzwiek();
+}
