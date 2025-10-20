2e6093d2756c063e3724059646fb95f23f549fb6
okejek323 <144665821+okejek323@users.noreply.github.com>
2025-10-20 10:04:58 +0200

    Update Program.cs

--- Postać i Statystyki/Program.cs
+++ Postać i Statystyki/Program.cs
@@ -1,9 +1,10 @@
 ﻿using Postać_i_Statystyki;
 
-Bohater Bohater_A = new Bohater();
-Bohater Bohater_B = new Bohater();
+Bohater Bohater_A = new Bohater.Wojownik();
+Bohater Bohater_B = new Bohater.Czarodziej();
+
+Bohater_B.Atakuj(Bohater_A);
+Bohater_A.Atakuj(Bohater_B);
 
 Bohater_A.Osoba("Tomek", "Wojownik");
 Bohater_B.Osoba("Michał", "Mag");
-
-Bohater_A.setZdrowie(120);
