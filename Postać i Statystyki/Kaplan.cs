e4d6f48608b84ec562e69ee22d6bb2b8f19a97a4
okejek323 <144665821+okejek323@users.noreply.github.com>
2025-11-13 09:05:25 +0100

    Add files via upload

---
+++ Postać i Statystyki/Kaplan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postać_i_Statystyki
+{
+
+    public interface Zaklinacz
+    {
+        void rzucCzar(string nazwaCzaru);
+        void regenerujMana(int wartosc);
+    }
+    public class Kaplan : Zaklinacz
+    {
+        public int mana;
+
+        public void rzucCzar(string nazwaCzaru)
+        {
+            if (mana >= 25)
+            {
+                Console.WriteLine($"Kapłan rzuca czar o nazwie: {nazwaCzaru}");
+                mana -= 25;
+                if (mana < 0) { mana = 0; }
+                Console.WriteLine($"Pozostało {mana} many");
+            }
+            else
+            {
+                Console.WriteLine($"Kapłan ma za mało many. Nie jest w stanie używać czarów.");
+            }
+        }
+
+        public void regenerujMana(int wartosc)
+        {
+            mana += wartosc;
+            Console.WriteLine($"Kapłan regeneruje mane i ma aktualnie {mana} many");
+        }
+    }
+}
